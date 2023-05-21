f2f0da1d21aecf7bc5f5d24157b1af6a670c096f
shaikat17 <shaikatpal56@gmail.com>
2023-05-22 02:00:34 +0600

    routes set

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import { Home } from "./pages";
+  About,
+  Cart,
+  CheckRoute,
+  Home,
+  Products,
+  SingleProduct,
+} from "./pages";
+import { Footer, Navbar, Sidebar } from "./components";
 
 function App() {
-
   return (
-    <>
-      <h1>SM CLOTH STORE</h1>
-      <Home />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/checkout" element={<CheckRoute />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
